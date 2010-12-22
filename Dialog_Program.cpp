9760569febd31cda118398b980ccbbe9da95bfaf
Ben Zonneveld <neurobender@users.sourceforge.net>
2010-12-22 23:09:59 +0100

    Started on a Dialog for editing of mirage programs
    Former-commit-id: 9b127d6f453827f88ddf4995531d01272a7f7308

---
+++ Dialog_Program.cpp
@@ -0,0 +1,172 @@
+// Dialog_Program.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "Mirage Editor.h"
+#include "Dialog_Program.h"
+
+
+// CMirProgram dialog
+
+IMPLEMENT_DYNAMIC(CMirProgram, CDialog)
+
+BEGIN_MESSAGE_MAP(CMirProgram, CDialog)
+	ON_WM_VSCROLL()
+	ON_EN_KILLFOCUS(IDC_LFO_FREQ_EDIT, &CMirProgram::OnKillFocusLfoFreqEdit)
+	ON_EN_KILLFOCUS(IDC_LFO_DEPTH_EDIT, &CMirProgram::OnKillFocusLfoDepthEdit)
+	ON_EN_KILLFOCUS(IDC_CUTOFF_EDIT, &CMirProgram::OnKillFocusCutoffEdit)
+	ON_EN_KILLFOCUS(IDC_RESO_EDIT, &CMirProgram::OnKillFocusResoEdit)
+	ON_EN_KILLFOCUS(IDC_FILT_ATT_EDIT, &CMirProgram::OnKillFocusFAttEdit)
+	ON_EN_KILLFOCUS(IDC_FILT_PEAK_EDIT, &CMirProgram::OnKillFocusFPeakEdit)
+	ON_EN_KILLFOCUS(IDC_FILT_DECAY_EDIT, &CMirProgram::OnKillFocusFDecEdit)
+	ON_EN_KILLFOCUS(IDC_FILT_SUS_EDIT, &CMirProgram::OnKillFocusFSusEdit)
+	ON_EN_KILLFOCUS(IDC_FILT_REL_EDIT, &CMirProgram::OnKillFocusFRelEdit)
+END_MESSAGE_MAP()
+
+CMirProgram::CMirProgram(CWnd* pParent /*=NULL*/)
+	: CDialog(CMirProgram::IDD, pParent)
+{
+}
+
+CMirProgram::~CMirProgram()
+{
+}
+
+void CMirProgram::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LFO_FREQ_SLID, m_lfo_freq_slider);
+	DDX_Control(pDX, IDC_LFO_FREQ_EDIT, m_lfo_freq_edit);
+	DDX_Control(pDX, IDC_LFO_DEPTH, m_lfo_depth_slider);
+	DDX_Control(pDX, IDC_LFO_DEPTH_EDIT, m_lfo_depth_edit);
+	DDX_Control(pDX, IDC_FILTER_CUTOFF, m_cutoff_slider);
+	DDX_Control(pDX, IDC_CUTOFF_EDIT, m_cutoff_edit);
+	DDX_Control(pDX, IDC_FILTER_RESONANCE, m_reso_slider);
+	DDX_Control(pDX, IDC_RESO_EDIT, m_reso_edit);
+	DDX_Control(pDX, IDC_FILTER_KEYB_TRACK, m_filter_tracking_slider);
+	DDX_Control(pDX, IDC_MONOMODE, m_mono_mode);
+	DDX_Control(pDX, IDC_FILTER_ENV_ATTACK, m_f_env_att_slider);
+	DDX_Control(pDX, IDC_FILTER_ENV_PEAK, m_f_env_peak_slider);
+	DDX_Control(pDX, IDC_FILTER_ENV_DECAY, m_f_env_dec_slider);
+	DDX_Control(pDX, IDC_FILTER_ENV_SUSTAIN, m_f_env_sus_slider);
+	DDX_Control(pDX, IDC_FILTER_ENV_RELEASE, m_f_env_rel_slider);
+	DDX_Control(pDX, IDC_FILT_ATT_EDIT, m_f_env_att_edit);
+	DDX_Control(pDX, IDC_FILT_PEAK_EDIT, m_f_env_peak_edit);
+	DDX_Control(pDX, IDC_FILT_DECAY_EDIT, m_f_env_dec_edit);
+	DDX_Control(pDX, IDC_FILT_SUS_EDIT, m_f_env_sus_edit);
+	DDX_Control(pDX, IDC_FILT_REL_EDIT, m_f_env_rel_edit);
+}
+
+BOOL CMirProgram::OnInitDialog()
+{
+	CString str;
+	CDialog::OnInitDialog();
+
+	/* Init Sliders for LFO */
+	InitSlider(m_lfo_freq_slider,m_lfo_freq_edit,0,99,10,10);
+	InitSlider(m_lfo_depth_slider,m_lfo_depth_edit,0,99,10,10);
+	SetSliderValue(m_lfo_freq_slider,m_lfo_freq_edit,0);
+	SetSliderValue(m_lfo_depth_slider,m_lfo_depth_edit,0);
+
+	/* Init Sliders for filter */
+	InitSlider(m_cutoff_slider,m_cutoff_edit,0,99,10,10);
+	InitSlider(m_reso_slider,m_reso_edit,0,40,10,10);
+	SetSliderValue(m_cutoff_slider,m_cutoff_edit,0);
+	SetSliderValue(m_reso_slider,m_reso_edit,0);
+
+	m_filter_tracking_slider.SetRange(0,4);
+	m_filter_tracking_slider.SetTicFreq(1);
+	m_filter_tracking_slider.SetPageSize(1);
+	m_filter_tracking_slider.SetPos(2);
+
+	/* Init Slider for Filter envelope part 1 */
+	InitSlider(m_f_env_att_slider,m_f_env_att_edit,0,31,5,5);
+	InitSlider(m_f_env_peak_slider,m_f_env_peak_edit,0,31,5,5);
+	InitSlider(m_f_env_dec_slider,m_f_env_dec_edit,0,31,5,5);
+	InitSlider(m_f_env_sus_slider,m_f_env_sus_edit,0,31,5,5);
+	InitSlider(m_f_env_rel_slider,m_f_env_rel_edit,0,31,5,5);
+	SetSliderValue(m_f_env_att_slider,m_f_env_att_edit,0);
+	SetSliderValue(m_f_env_peak_slider,m_f_env_peak_edit,0);
+	SetSliderValue(m_f_env_dec_slider,m_f_env_dec_edit,0);
+	SetSliderValue(m_f_env_sus_slider,m_f_env_sus_edit,0);
+	SetSliderValue(m_f_env_rel_slider,m_f_env_rel_edit,0);
+
+	return TRUE;
+}
+
+
+void CMirProgram::OnVScroll(UINT SBCode, UINT nPos, CScrollBar *pScrollBar)
+{
+	CString str;
+	/* LFO Sliders */
+	SetSlider(m_lfo_freq_slider,m_lfo_freq_edit);
+	SetSlider(m_lfo_depth_slider,m_lfo_depth_edit);
+
+	/* Filter Sliders */
+	SetSlider(m_cutoff_slider,m_cutoff_edit);
+	SetSlider(m_reso_slider,m_reso_edit);
+
+	/* Filter Envelope Sliders part 1 */
+	SetSlider(m_f_env_att_slider,m_f_env_att_edit);
+	SetSlider(m_f_env_peak_slider,m_f_env_peak_edit);
+	SetSlider(m_f_env_dec_slider,m_f_env_dec_edit);
+	SetSlider(m_f_env_sus_slider,m_f_env_sus_edit);
+	SetSlider(m_f_env_rel_slider,m_f_env_rel_edit);
+
+	CDialog::OnVScroll(SBCode, nPos, pScrollBar);
+}
+
+void CMirProgram::OnKillFocusLfoFreqEdit()
+{
+	CString str;
+//	m_lfo_freq_edit.GetWindowTextA(str);
+	SetSliderFromEdit(m_lfo_freq_slider,m_lfo_freq_edit);
+}
+
+void CMirProgram::OnKillFocusLfoDepthEdit()
+{
+	CString str;
+	SetSliderFromEdit(m_lfo_depth_slider,m_lfo_depth_edit);
+}
+
+void CMirProgram::OnKillFocusCutoffEdit()
+{
+	CString str;
+	SetSliderFromEdit(m_cutoff_slider,m_cutoff_edit);
+}
+
+void CMirProgram::OnKillFocusResoEdit()
+{
+	CString str;
+	SetSliderFromEdit(m_reso_slider,m_reso_edit);
+}
+
+void CMirProgram::OnKillFocusFAttEdit()
+{
+	CString str;
+	SetSliderFromEdit(m_f_env_att_slider,m_f_env_att_edit);
+}
+
+void CMirProgram::OnKillFocusFPeakEdit()
+{
+	CString str;
+	SetSliderFromEdit(m_f_env_peak_slider,m_f_env_peak_edit);
+}
+
+void CMirProgram::OnKillFocusFDecEdit()
+{
+	CString str;
+	SetSliderFromEdit(m_f_env_dec_slider,m_f_env_dec_edit);
+}
+
+void CMirProgram::OnKillFocusFSusEdit()
+{
+	CString str;
+	SetSliderFromEdit(m_f_env_sus_slider,m_f_env_sus_edit);
+}
+
+void CMirProgram::OnKillFocusFRelEdit()
+{
+	CString str;
+	SetSliderFromEdit(m_f_env_rel_slider,m_f_env_rel_edit);
+}
--- Dialog_Program.h
+++ Dialog_Program.h
@@ -1,7 +1,23 @@
 #pragma once
-
+#include "afxwin.h"
+#include "afxcmn.h"
 
 // CMirProgram dialog
+
+#define InitSlider(slider_ctrl,edit_ctrl,min,max,tick_freq,pagesize) \
+	slider_ctrl.SetRange(min,max); \
+	slider_ctrl.SetTicFreq(tick_freq); \
+	slider_ctrl.SetPageSize(pagesize);
+#define SetSlider(slider_ctrl,edit_ctrl) \
+	str.Format(_T("%d"),slider_ctrl.GetRangeMax()-slider_ctrl.GetPos()); \
+	edit_ctrl.SetWindowTextA(str);
+#define SetSliderValue(slider_ctrl,edit_ctrl,value) \
+	slider_ctrl.SetPos(slider_ctrl.GetRangeMax()-value); \
+	SetSlider(slider_ctrl,edit_ctrl);
+#define SetSliderFromEdit(slider_ctrl,edit_ctrl) \
+	edit_ctrl.GetWindowTextA(str); \
+	slider_ctrl.SetPos(slider_ctrl.GetRangeMax()-atoi(str)); \
+	SetSlider(slider_ctrl,edit_ctrl);
 
 class CMirProgram : public CDialog
 {
@@ -10,12 +26,48 @@ class CMirProgram : public CDialog
 public:
 	CMirProgram(CWnd* pParent = NULL);   // standard constructor
 	virtual ~CMirProgram();
+	virtual BOOL OnInitDialog();
 
 // Dialog Data
 	enum { IDD = IDD_PROGRAM_SETTINGS };
-
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-
 	DECLARE_MESSAGE_MAP()
+
+public:
+	afx_msg void CMirProgram::OnVScroll(UINT SBCode, UINT nPos, CScrollBar *pScrollBar);
+
+	// Keyboard Params
+	CSliderCtrl m_lfo_freq_slider;
+	CSliderCtrl m_lfo_depth_slider;
+	CSliderCtrl m_cutoff_slider;
+	CSliderCtrl m_reso_slider;
+	CSliderCtrl m_filter_tracking_slider;
+	CEdit m_lfo_freq_edit;
+	CEdit m_lfo_depth_edit;
+	CEdit m_cutoff_edit;
+	CEdit m_reso_edit;
+	CButton m_mono_mode;
+
+	afx_msg void OnKillFocusLfoFreqEdit();
+	afx_msg void OnKillFocusLfoDepthEdit();
+	afx_msg void OnKillFocusCutoffEdit();
+	afx_msg void OnKillFocusResoEdit();
+
+	// Filter Envelope
+	CSliderCtrl m_f_env_att_slider;
+	CSliderCtrl m_f_env_peak_slider;
+	CSliderCtrl m_f_env_dec_slider;
+	CSliderCtrl m_f_env_sus_slider;
+	CSliderCtrl m_f_env_rel_slider;
+	CEdit m_f_env_att_edit;
+	CEdit m_f_env_peak_edit;
+	CEdit m_f_env_dec_edit;
+	CEdit m_f_env_sus_edit;
+	CEdit m_f_env_rel_edit;
+	afx_msg void OnKillFocusFAttEdit();
+	afx_msg void OnKillFocusFPeakEdit();
+	afx_msg void OnKillFocusFDecEdit();
+	afx_msg void OnKillFocusFSusEdit();
+	afx_msg void OnKillFocusFRelEdit();
 };
