fb3e32fd0e3a8f0d6ea5cad2494884cb2c337677
Ben Zonneveld <neurobender@users.sourceforge.net>
2010-12-31 00:35:56 +0100

    Changed the Config dialog to a tabbed dialog
    TODO: Implement functionality
    
    Former-commit-id: 7d2e58bfc02e17dd8bcf2cbe87291ea636d99f6f

---
+++ Tabby.h
@@ -0,0 +1,59 @@
+#if !defined(AFX_TABBY_H__60E75989_9A76_4ADE_8AFF_F5A88D8962A4__INCLUDED_)
+#define AFX_TABBY_H__60E75989_9A76_4ADE_8AFF_F5A88D8962A4__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// Tabby.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CTabby window
+
+class CTabby : public CTabCtrl {
+	CWnd* m_Parent;
+	int m_PreviousTab;
+	CTabGeneralKeyboard m_TabOneDialog;
+	CTabSamplingConfig m_TabTwoDialog;
+	CTabSequencer m_TabThreeDialog;
+	CTabMidiConfig m_TabFourDialog;
+	CDialog *m_pDialogs[4];
+	int m_MaxWidth, m_MaxHeight;
+
+	BOOL CreateTab(CDialog *, UINT, int);
+
+public:
+	CTabby();
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CTabby)
+	protected:
+	virtual void PreSubclassWindow();
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CTabby() {};
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CTabby)
+	afx_msg void OnSelChange(NMHDR* pNMHDR, LRESULT* pResult);
+	//}}AFX_MSG
+
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_TABBY_H__60E75989_9A76_4ADE_8AFF_F5A88D8962A4__INCLUDED_)
