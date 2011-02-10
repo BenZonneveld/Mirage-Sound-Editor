a820b4d1884ed8ce49d66493916a20dccdb42034
Ben Zonneveld <neurobender@users.sourceforge.net>
2011-02-10 23:34:59 +0100

    Created a SndMem Class for sndobj
    Former-commit-id: 217c0d8d40915c9c7010aa9b9b46e17fb0e72a01

---
+++ SndMem.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <errno.h>
+#include <SndObj/SndIO.h>
+
+class SndMem : public SndIO{
+
+protected:
+	short m_mode;
+	long	m_datapos;
+	long	m_dataframes;
+	char *	m_wavedata;
+	long	m_datasize;
+	long	m_buffsize;
+	char* m_buffer;
+	char* m_cp;
+	int		m_eof;
+
+public:
+	short GetMode() { return m_mode; }
+
+	long GetDataFrames() { return m_dataframes; }
+	SndMem(char * wavdata, long wavsize, short mode, short channels=1, short bits=8, SndObj** inputlist=0, int vecsize=DEF_VECSIZE, float sr=DEF_SR);
+	~SndMem();
+	short Read();
+	short Write();
+	int Eof() { return(!(m_datapos < m_datasize)); }
+	char * ErrorMessage();
+	float Output(int pos) { return m_wavedata[pos]; }
+	float Output(int pos, int channel) {
+		return m_wavedata[pos]; }
+};
