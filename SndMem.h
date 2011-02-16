de78640b67a222a583f0bfd2156e936b91865beb
Ben Zonneveld <neurobender@users.sourceforge.net>
2011-02-16 23:35:53 +0100

    sndobj 8 bit seems to be broken.
    I don't want to spend any more time on it.
    Workaround: convert samples to 16 bit before dsp and back to 8 bit after dsp

--- SndMem.h
+++ SndMem.h
@@ -8,18 +8,19 @@ protected:
 	short m_mode;
 	long	m_datapos;
 	long	m_dataframes;
-	char unsigned *	m_wavedata;
+	short *	m_wavedata;
 	long	m_datasize;
-	long	m_buffsize;
-	char* m_buffer;
-	char* m_cp;
+//	long	m_buffsize;
+//	char* m_buffer;
+//	char* m_cp;
+//	short *m_sp;
 	int		m_eof;
 
 public:
 	short GetMode() { return m_mode; }
 
 	long GetDataFrames() { return m_dataframes; }
-	SndMem(char unsigned * wavdata, long wavsize, short mode, short channels=1, short bits=8, SndObj** inputlist=0, int vecsize=DEF_VECSIZE, float sr=DEF_SR);
+	SndMem(short * wavdata, long wavsize, short mode, short channels=1, short bits=8, SndObj** inputlist=0, int vecsize=DEF_VECSIZE, float sr=DEF_SR);
 	~SndMem();
 	short Read();
 	short Write();
