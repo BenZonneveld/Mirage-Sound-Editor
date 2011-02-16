fe5146d2c3e430e351d829c90e3af9382b79fdd8
Ben Zonneveld <neurobender@users.sourceforge.net>
2011-02-16 23:35:53 +0100

    sndobj 8 bit seems to be broken.
    I don't want to spend any more time on it.
    Workaround: convert samples to 16 bit before dsp and back to 8 bit after dsp
    
    Former-commit-id: 90acfcfad1996c7b3c126139416ebc4ebd824092

--- SndMem.cpp
+++ SndMem.cpp
@@ -1,12 +1,12 @@
 #include "stdafx.h"
 #include "SndMem.h"
 
-SndMem::SndMem(char unsigned * wavdata, long wavsize, short mode, short channels, short bits, SndObj** inputlist, int vecsize, float sr) : SndIO(channels,bits,inputlist,vecsize,sr){
+SndMem::SndMem(short * wavdata, long wavsize, short mode, short channels, short bits, SndObj** inputlist, int vecsize, float sr) : SndIO(channels,bits,inputlist,vecsize,sr){
 	m_wavedata = wavdata;
 	m_datasize = wavsize;
 	m_channels = 1;
 	
-	if ( m_bits > 0){
+/*	if ( m_bits > 0){
 		m_buffsize = ( m_bits/8)*m_samples;
 		if (!(m_buffer = new char[m_buffsize])){
 			m_error=11;
@@ -14,13 +14,14 @@ SndMem::SndMem(char unsigned * wavdata, long wavsize, short mode, short channels
 		}
 
 		m_cp=(char *) m_buffer;
-	}
+		m_sp =(short *)m_buffer;
+	}*/
 	m_datapos = 0;
 }
 
 SndMem::~SndMem()
 {
-	delete[] m_buffer;
+//	delete[] m_buffer;
 }
 
 short SndMem::Read()
@@ -35,7 +36,7 @@ short SndMem::Read()
 			{
 				items++;
 				items2++;
-				m_output[m_vecpos]= static_cast<float>(m_wavedata[m_vecpos+m_datapos]); //(float) m_cp[m_vecpos];
+				m_output[m_vecpos]= static_cast<float>(m_wavedata[m_vecpos+m_datapos]); 
 			} else {
 				items2++;
 				m_output[m_vecpos]= 0.f;
@@ -49,6 +50,20 @@ short SndMem::Read()
 
 short SndMem::Write()
 {
+	int i,n;
+	int items=0;
+	for (m_vecpos=n=0; m_vecpos < m_samples ; m_vecpos += m_channels)
+	{
+		for (i=0; i< m_channels ; i++)
+		{
+			if (m_IOobjs[i])
+			{
+				m_wavedata[m_datapos+m_vecpos+i]=static_cast<short>(m_IOobjs[i]->Output(m_vecpos));
+				items++;
+			}
+		}
+	}
+	m_datapos += items;
 	return 0;
 }
 
