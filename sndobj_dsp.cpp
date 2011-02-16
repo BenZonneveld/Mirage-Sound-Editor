ef7bb9e213e0e272bdd27c6953eb6475b8d8763c
Ben Zonneveld <neurobender@users.sourceforge.net>
2011-02-16 23:35:53 +0100

    sndobj 8 bit seems to be broken.
    I don't want to spend any more time on it.
    Workaround: convert samples to 16 bit before dsp and back to 8 bit after dsp
    
    Former-commit-id: 523f077dc579aff7ff3506213b363a1d33e09c56

--- sndobj_dsp.cpp
+++ sndobj_dsp.cpp
@@ -1,9 +1,19 @@
 #include "stdafx.h"
+#include "samplerate.h"
 #include <SndObj/AudioDefs.h>
 #include "SndMem.h"
 
 void resynthesize(CString Pathname,char unsigned * wavedata, long samplesize, float sr, unsigned char bit_depth)
 {
+	float *lpFloatOut;
+	short *short_array;
+	lpFloatOut=(float *)malloc(samplesize*sizeof(float));
+	short_array=(short * )malloc(samplesize*sizeof(short));
+
+
+	src_unchar_to_float_array(wavedata, lpFloatOut, (int)samplesize);
+	src_float_to_short_array(lpFloatOut, short_array, (int)samplesize);
+
 	int encoding=BYTESAM;
 /*	char * path=Pathname.GetBuffer(Pathname.GetLength());
 	SndWave input(path, READ,1,8,0,0,DEF_VECSIZE,sr);
@@ -18,22 +28,26 @@ void resynthesize(CString Pathname,char unsigned * wavedata, long samplesize, fl
 	}
 
 	encoding = BYTESAM;*/
+	int fftsize = 2048;
 	int count=0;
 	int maxcount=20;
 	HammingTable hanning(1024,0.5f);
 
-	SndMem input(wavedata,samplesize,READ,1,8,0,DEF_VECSIZE,sr);
-	SndRTIO output(2,SND_OUTPUT,DEF_BSIZE,DEF_PERIOD,encoding,0,DEF_VECSIZE,sr);
+	SndMem input(short_array,samplesize,READ,1,16,0,DEF_VECSIZE,sr);
+	SndMem sndout(short_array, samplesize, OVERWRITE,1,	16,0,DEF_VECSIZE,sr);
+
+	SndRTIO output(2,SND_OUTPUT,DEF_BSIZE,DEF_PERIOD,SHORTSAM,0,DEF_VECSIZE,sr);
 	SndIn  in(&input,1);//,DEF_VECSIZE,sr);
 //	ButtHP highpass(2500.0f,&in,0,DEF_VECSIZE,sr);
-	PVA anal(&hanning,&in,1.0f,DEF_FFTSIZE,DEF_VECSIZE,sr);
-	PVBlur blur(&anal,0.05f,DEF_VECSIZE,DEF_FFTSIZE,sr);
-	PVS synth(&hanning,&blur);//,DEF_FFTSIZE,DEF_VECSIZE,sr);
+	PVA anal(&hanning,&in,.5f,fftsize,DEF_VECSIZE,sr);
+	PVBlur blur(&anal,0.05f,DEF_VECSIZE,fftsize,sr);
+	PVS synth(&hanning,&blur,fftsize,DEF_VECSIZE,sr);
 //	ButtLP synth(2000.f,&in,0,DEF_VECSIZE,sr);
 //	Gain outgain(-1.0f,&synth,DEF_VECSIZE,sr);
 
 	output.SetOutput(1,&synth);
 	output.SetOutput(2,&synth);
+	sndout.SetOutput(1,&synth);
 
 	while(!input.Eof())
 	{
@@ -45,5 +59,10 @@ void resynthesize(CString Pathname,char unsigned * wavedata, long samplesize, fl
 		synth.DoProcess();
 //		outgain.DoProcess();
 		output.Write();	
+		sndout.Write();
 	}
+	src_short_to_float_array(short_array,lpFloatOut, (int)samplesize);
+	src_float_to_unchar_array(lpFloatOut,wavedata,(int)samplesize);
+	free(short_array);
+	free(lpFloatOut);
 }
