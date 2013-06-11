9235ddafecbf612427c5b8c363d950d3879b33d3
Ben Zonneveld <neurobender@users.sourceforge.net>
2013-06-12 00:41:13 +0200

    Got the original key selection working.
    Now there seems to be a problem with the allocated memory for the sample dump sysex transmit.
    
    
    Former-commit-id: aae058fdb54f3988806045c10b6ba891e49e5f3a

--- Dialog_OrigKey.cpp
+++ Dialog_OrigKey.cpp
@@ -33,6 +33,8 @@ BOOL COrigKey::OnInitDialog()
 	CDialog::OnInitDialog();
 
 	m_OrigKey = 255;
+	m_NoteOff = false;
+	m_NoteOn = false;
 
 	/* Create the piano Keyboard */
 	m_Keys.Initialize(CMIDIKeyboard::LOW_NOTE, CMIDIKeyboard::HIGH_NOTE);
@@ -46,39 +48,52 @@ BOOL COrigKey::OnInitDialog()
 
 void COrigKey::OnNoteOn(CPianoCtrl &PianoCtrl, unsigned char NoteId)
 {
-	if ( m_OrigKey != ( NoteId - 0x23) && m_OrigKey < 255 )
+/*	if ( m_OrigKey != ( NoteId - 0x23) && m_OrigKey < 255 )
 	{
 		COLORREF NoteColor=PianoCtrl.GetNoteOnColor();
 		PianoCtrl.SetNoteOnColor(RGB(0, 250, 150));
 		m_Keys.NoteOn(m_OrigKey + 0x23);
 		PianoCtrl.SetNoteOnColor(NoteColor);
-	}
+	}*/
 	Invalidate(FALSE);
 	UpdateWindow();
 }
 
 void COrigKey::OnNoteOff(CPianoCtrl &PianoCtrl, unsigned char NoteId)
 {
-	if ( m_OrigKey < 255 && m_OrigKey != (NoteId - 0x23))
+	if ( m_NoteOff == false )
 	{
-		COLORREF NoteColor=PianoCtrl.GetNoteOnColor();
-		PianoCtrl.SetNoteOnColor(RGB(0, 250, 150));
+		if ( m_OrigKey < 255 || m_OrigKey != (NoteId - 0x23))
+		{
+			COLORREF NoteColor=PianoCtrl.GetNoteOnColor();
+			PianoCtrl.SetNoteOnColor(RGB(0, 250, 150));
 
-		m_Keys.DetachListener(*this);
-		m_Keys.NoteOff(m_OrigKey + 0x23);
-		m_Keys.NoteOn(NoteId);
-		/* Attach ourselves to the piano control so we will be notified */
-		m_Keys.AttachListener(*this);
-
-		PianoCtrl.SetNoteOnColor(NoteColor);
+			if ( m_OrigKey < 255)
+			{
+				m_NoteOff = true;
+				m_Keys.NoteOff(m_OrigKey + 0x23);
+			}
+			m_Keys.NoteOn(NoteId);
+			PianoCtrl.SetNoteOnColor(NoteColor);
+		}
+		m_OrigKey = NoteId - 0x23;
+	} else {
+		m_NoteOff = false;
 	}
-	m_OrigKey = NoteId - 0x23;
 	Invalidate(FALSE);
 	UpdateWindow();
 }
 
+void COrigKey::OnButtonOk()
+{
+	theApp.m_LastNote = m_OrigKey;
+
+	OnOK();
+}
+
 BEGIN_MESSAGE_MAP(COrigKey, CDialog)
 		ON_WM_DRAWITEM()
+		ON_BN_CLICKED(IDORIGKEYOK, &COrigKey::OnButtonOk)
 	//{{AFX_MSG_MAP(COrigKey)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
