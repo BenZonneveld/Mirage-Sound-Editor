df3c486944d2607a0be36f512a9ed2501a300bbd
Ben Zonneveld <neurobender@users.sourceforge.net>
2013-06-15 00:08:42 +0200

    Tried sending all parms in one go. Didn't work.
    So back to the old method of sending value up/down keypad commands
    
    
    Former-commit-id: 4cc36bdde1603c887ba873e1af3f39ac0f5f6013

--- MirageParameters.cpp
+++ MirageParameters.cpp
@@ -29,22 +29,23 @@ void ChangeParameter(const char * Name, unsigned char Parameter, unsigned char V
 
 	ParmChange[6] = ParmDecimal;
 	ParmChange[7] = ParmDigit;
+	ResetEvent(midi_in_event);
 	SendData(ParmChange);
 ParmChangeLoop:
-	SendData(GetCurrentValue);
-	ResetEvent(midi_in_event);
 //	SendData(GetCurrentValue);
+	ResetEvent(midi_in_event);
+	SendData(GetCurrentValue);
 	for(int c=0; c<no_parms ; c++)
 	{
-		DWORD wait_state = WaitForSingleObject(midi_in_event,24);
+		DWORD wait_state = WaitForSingleObject(midi_in_event,50);
 		if (wait_state == WAIT_TIMEOUT)
 		{
 			return;
 		}
-		if ( (c+1) < no_parms)
-		{
-			ResetEvent(midi_in_event);
-		}
+//		if ( (c+1) < no_parms)
+//		{
+//			ResetEvent(midi_in_event);
+//		}
 	}
 	// Update the progressbar
 	if (progress_val_set == false )
