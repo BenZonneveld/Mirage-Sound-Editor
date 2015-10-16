28cd83a691407b7abd3db55836edec5c65c890a5
Ben Zonneveld <neurobender@users.sourceforge.net>
2015-10-16 22:46:25 +0200

    Still working on it
    Former-commit-id: bcbb89cffe071930cea2e2762111e8402376500b

--- sysex_logger.cpp
+++ sysex_logger.cpp
@@ -5,15 +5,10 @@
 
 void sysex_logmsg(const unsigned char * ptr,DWORD BytesRecorded, BOOL io_dir)
 {
-//	strcpy_s(theApp.m_midimonitorstring.c_str(), (size_t)BytesRecorded, ptr);
-
 	theApp.m_midimonitorstring.assign((const char *)ptr, BytesRecorded);
 
 	theApp.cds.dwData = io_dir; // can be anything
 	theApp.cds.cbData = BytesRecorded;
-	theApp.cds.lpData =  (LPVOID)theApp.m_midimonitorstring.c_str();
-//	theApp.m_pMidiDoc->PutData(string("SysexMsg"), io_dir);
-//	theApp.m_pMidiDoc->ParseSysex(NULL, (LPARAM)(LPVOID)&theApp.cds);
-//	theApp.m_MidiMonitorThread->PostThreadMessage(WM_PARSESYSEX, NULL, (LPARAM)(LPVOID)&theApp.cds);
+	theApp.cds.lpData =  (LPVOID)theApp.m_midimonitorstring.data();
 	theApp.m_MidiMonitorThread->ThreadMessage(WM_PARSESYSEX, NULL, (LPARAM)(LPVOID)&theApp.cds);
 }
