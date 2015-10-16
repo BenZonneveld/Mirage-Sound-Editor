b45adacd0d73bfc5650021eed2767a1b58c302e7
Ben Zonneveld <neurobender@users.sourceforge.net>
2015-10-16 22:46:25 +0200

    Still working on it
    Former-commit-id: da7a1a8dfcbe5ace22ee7d34c725ff5b506a524c

--- SendSysex.cpp
+++ SendSysex.cpp
@@ -16,12 +16,12 @@ void SendLongData(unsigned char *sysEx, UINT SysXsize)
 {
 	sysex_logmsg(&sysEx[0],SysXsize, MIDIMON_OUT);
 	theApp.m_OutDevice.SendMsg((LPSTR)&sysEx[0],SysXsize);
-	theApp.m_pMidiDoc->OnUpdateAllViews();
+//	theApp.m_pMidiDoc->OnUpdateAllViews();
 }
 
 void SendData(unsigned char *sysEx)
 {
-	theApp.m_OutDevice.SendMsg((LPSTR)&sysEx[1],sysEx[0]);
 	sysex_logmsg(&sysEx[1],sysEx[0], MIDIMON_OUT);
-	theApp.m_pMidiDoc->OnUpdateAllViews();
+	theApp.m_OutDevice.SendMsg((LPSTR)&sysEx[1],sysEx[0]);
+//	theApp.m_pMidiDoc->OnUpdateAllViews();
 }
