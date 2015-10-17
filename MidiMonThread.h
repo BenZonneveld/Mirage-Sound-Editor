b089183f59efb89f27eb92bdc0d1219f744560a9
Ben Zonneveld <neurobender@users.sourceforge.net>
2015-10-17 22:20:48 +0200

    Working on a new implemention of gui threads. Hope this works beter.
    Former-commit-id: 86a6668ea1572719ffa960e9b332e074eb6df630

---
+++ MidiMonThread.h
@@ -0,0 +1,32 @@
+Class CMidiMonThread : public CWinThread
+{
+	DECLARE_DYNCREATE(CMidiMonThread)
+
+protected:
+	CMidiMonThread(); // protected constructor used by dynamic creation
+public:
+	CMidiMonThread(HWND hwndParent);
+	void operator delete(void* p);
+
+// Attributes
+public:
+	static HANDLE m_hEventMidiMonThreadKilled;
+
+protected:
+	HWND				m_hwndParent;
+	CMidiMonWnd	m_wndMidiMon;
+
+// Operations
+public:
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+protected:
+	virtual ~CMidiMonThread();
+
+	DECLARE_MESSAGE_MAP()
+};
