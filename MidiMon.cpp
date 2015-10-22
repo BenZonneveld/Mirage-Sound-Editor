04f4980f917827183b7c732af6bdf128a6df6ef1
Ben Zonneveld <neurobender@users.sourceforge.net>
2015-10-22 22:56:28 +0200

    Before change to CScrollView

--- MidiMon.cpp
+++ MidiMon.cpp
@@ -35,70 +35,6 @@ BOOL CMidiMonChildWnd::Create(LPCTSTR szTitle, LONG style /* = 0 */, const RECT&
 	return TRUE;
 }
 
-BOOL CMidiMonChildWnd::OnCmdMsg(UINT nID, int nCode, void* pExtra,
-		AFX_CMDHANDLERINFO* pHandlerInfo)
-{
-	// The default CFrameWnd::OnCmdMsg function is responsible for
-	// dispatching menu commands to ON_COMMAND handlers and for
-	// initializing menus and toolbars via ON_UPDATE_COMMAND_UI
-	// handlers.  For the MDI child window, all such dispatching
-	// and command user interface initialization are done in the
-	// main application thread.  The CBounceWnd object processes
-	// user interface events in a separate thread.  This override,
-	// CBounceMDIChildWnd::OnCmdMsg, delegates command handling to
-	// the child CBounceWnd window.  It cannot do this delegation
-	// by simply calling CBounceWnd::OnCmdMsg, because doing so
-	// would defeat the design of handling CBounceWnd user interface
-	// events in the separate thread.  If CBounceMDIChildWnd::OnCmdMsg
-	// simply called CBounceWnd::OnCmdMsg, then the command handling
-	// would be processed in the same main application thread as that
-	// of the MDI child window.
-	//
-	// A common and safe way to make a call from one thread to another
-	// is to call SendMessage for a window whose message pump runs in
-	// a second thread.  That is how CBounceMDIChildWnd::OnCmdMsg is
-	// implemented.  All of the OnCmdMsg parameters are gathered into
-	// an application-defined (but re-usable) COnCmdMsg structure,
-	// that is sent by reference to the CBounceWnd window via the
-	// lParam of SendMessage.
-
-
-	CWnd* pMidiMonWnd = (CMidiMonWnd*)GetDlgItem(IDC_MIDIMON_WND);
-	if (pMidiMonWnd == NULL)
-		return FALSE; // child CBounceWnd not created yet.
-
-	// It is safe to create the COnCmdMsg on the stack because
-	// it will not be deleted until the SendMessage call has
-	// synchronously executed.
-
-	COnCmdMsg oncmdmsg;
-	oncmdmsg.m_nID =    nID;
-	oncmdmsg.m_nCode = nCode;
-	oncmdmsg.m_pExtra = pExtra;
-	oncmdmsg.m_pHandlerInfo = pHandlerInfo;
-
-	// In addition to passing all of the OnCmdMsg parameters to
-	// the CBounceWnd window via the SendMessage lParam, it is also
-	// necessary to forward the implicit wParam originally passed
-	// in the WM_COMMAND message for the menu command.  The original
-	// WM_COMMAND wParam is not a parameter of OnCmdMsg, but it can
-	// be obtained by calling GetCurrentMessage.  The wParam is needed by
-	// the CBounceWnd::OnColor command handler to distinguish which
-	// menu item, i.e., which color, was selected from the Color menu.
-	//
-	// The original single thread MDI sample application, the
-	// CBounceWnd::OnColor command handler calls GetCurrentMessage
-	// to retrieve the wParam from the WM_COMMAND message.  To satisfy
-	// that implementation of CBounceWnd::OnColor, we call
-	// GetCurrentMessage() and forward the wParam in the user-defined
-	// WM_USER_ONCMDMSG message.  Alternatively, we could have
-	// called GetMessageMsg and passed the wParam as an additional
-	// member of the COnCmdMsg structure.
-
-	return (BOOL)pMidiMonWnd->SendMessage(WM_USER_ONCMDMSG,
-		GetCurrentMessage()->wParam, (LPARAM)&oncmdmsg);
-}
-
 BOOL CMidiMonChildWnd::DestroyWindow()
 {
 	OnPrepareToClose();
@@ -120,20 +56,11 @@ LRESULT CMidiMonChildWnd::OnPrepareToClose(WPARAM, LPARAM)
 
 LRESULT CMidiMonChildWnd::OnPutData(WPARAM wParam, LPARAM lParam)
 {
-//	MSG msg;
-
-//	PeekMessage(&msg, NULL, WM_MIDIMONITOR,WM_MIDIMONITOR, PM_REMOVE);
 	m_pMidiMonWnd = (CMidiMonWnd*)GetDlgItem(IDC_MIDIMON_WND);
 	if (m_pMidiMonWnd == NULL)
 		return FALSE; // child CBounceWnd not created yet.
 
 	return (BOOL)m_pMidiMonWnd->SendMessage(WM_MM_PUTDATA, wParam, lParam);
-
-	//string mydata;
-	//COPYDATASTRUCT* pcds = (COPYDATASTRUCT*)lParam;
-	//mydata=(LPCTSTR)(pcds->lpData);
-//	m_pMidiDoc->PutData(mydata, pcds->dwData);
-
 }
 
 BEGIN_MESSAGE_MAP(CMidiMonWnd, CWnd)
@@ -181,27 +108,74 @@ int CMidiMonWnd::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	return 0;
 }
 
-void CMidiMonWnd::OnDraw(CDC* pDC)
+void CMidiMonWnd::OnPaint()
 {
+  CPaintDC dc(this);
+	CDC dcMem;
+
+	dcMem.CreateCompatibleDC(&dc);
+
+	CSize sizeTotal;
+	POINT scrollPos;
+	CRect rect;
+
+
+	sizeTotal.cx = 50;
+	sizeTotal.cy = m_nLineHt * m_pMidiDoc->GetSize();
+
+	dcMem.SetTextColor(RGB(127,127,0));
+	dcMem.SetBkColor(::GetSysColor(COLOR_WINDOW));
+	GetClientRect(rect);
+	dcMem.DrawText(_T("Hello, World!"), -1, rect,
+		DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+	scrollPos.x = 0;
+	scrollPos.y = m_pMidiDoc->GetSize();
+
+//	SetScrollSizes(MM_TEXT, sizeTotal);
+
+//	ScrollToPosition(scrollPos);
+
+	CFont *pOldFont;
+	
+	int ct;
+
+	dcMem.SetBkMode(TRANSPARENT);
+	
+	int start = 0; //GetDeviceScrollPosition().y /m_nLineHt -1;
+	if (start < 0) start = 0;
+
+	CRect rcClient;
+
+	GetClientRect(&rcClient);
+
+	ct = start + rcClient.Height()/m_nLineHt + 3;
+
+	if( ct > m_pMidiDoc->GetSize() ) ct=m_pMidiDoc->GetSize();
+	
+	pOldFont = dcMem.SelectObject(&m_ftTimes);
+	
+	for(int i=start;i<ct;i++)
+//	for(int i=ct-1;i>=start;i--)
+	{
+		if ( m_pMidiDoc->GetIO(i) )
+		{
+			dcMem.SetTextColor(RGB(127,0,0));
+		} else {
+			dcMem.SetTextColor(RGB(0,127,0));
+		}
+		dcMem.TextOut(0,i * m_nLineHt,&m_pMidiDoc->GetData(i)[0]);
+	}
+	
+	dcMem.SelectObject(pOldFont);
+	dcMem.DeleteDC();
 }
-// CMidiMonWnd commands
-//LRESULT CMidiMonWnd::OnDelegatedCmdMsg(WPARAM, LPARAM lParam)
-//{
-//	COnCmdMsg* pOnCmdMsg = (COnCmdMsg*)lParam;
-//	return CWnd::OnCmdMsg(pOnCmdMsg->m_nID, pOnCmdMsg->m_nCode, pOnCmdMsg->m_pExtra,
-//			pOnCmdMsg->m_pHandlerInfo);
-//}
 
 LRESULT CMidiMonWnd::OnPutData(WPARAM wParam, LPARAM lParam)
 {
-//	MSG msg;
-
-//	PeekMessage(&msg, NULL, WM_MIDIMONITOR,WM_MIDIMONITOR, PM_REMOVE);
-
 	string mydata;
 	COPYDATASTRUCT* pcds = (COPYDATASTRUCT*)lParam;
 	mydata=(LPCTSTR)(pcds->lpData);
 	m_pMidiDoc->PutData(mydata, pcds->dwData);
-//	OnDraw();
+	OnPaint();
 	return true;
 }
