6f16487cb8db961a3f35cebdfe281e998f868a00
Ben Zonneveld <neurobender@users.sourceforge.net>
2015-11-03 00:32:47 +0100

    Can't seem to get it working

--- MidiMon.h
+++ MidiMon.h
@@ -26,8 +26,6 @@ public:
 	CWnd* CreateView(CCreateContext* pContext, UINT nID);
 	BOOL OnCreateClient(LPCREATESTRUCT, CCreateContext* pContext);
 	virtual BOOL DestroyWindow();
-/*	BOOL LoadFrame(UINT nIDResource, DWORD dwDefaultStyle,
-		CWnd* pParentWnd, CCreateContext* pContext);*/
 protected:
 	afx_msg LRESULT OnPrepareToClose(WPARAM wParam = 0, LPARAM lParam = 0);
 	afx_msg LRESULT OnPutData(WPARAM wParam, LPARAM lParam);
