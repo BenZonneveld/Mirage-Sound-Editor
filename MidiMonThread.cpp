31992b594c63e86dd0d242a94501de52a716a3e7
Ben Zonneveld <neurobender@users.sourceforge.net>
2015-11-09 23:31:06 +0100

    More info in the midi monitor

--- MidiMonThread.cpp
+++ MidiMonThread.cpp
@@ -3,6 +3,7 @@
 #include "Midi View.h"
 #include "MidiMonThread.h"
 #include "Mirage Editor.h"
+#include "Nybble.h"
 
 HANDLE CMidiMonThread::m_hEventMidiMonThreadKilled;
 
@@ -117,6 +118,40 @@ void CMidiMonThread::OnPutData(WPARAM wParam, LPARAM lParam)
 
 void CMidiMonThread::OnParseSysex(WPARAM wParam, LPARAM lParam)
 {
+	char ConfigParams[][29] = {
+		"Dummy Byte",
+		"Master Tune",
+		"Pitch Bend Range",
+		"Velocity Sensitivity",
+		"Upper/Lower Balance",
+		"Program Link Switch",
+		"Sample Time Adjust",
+		"Input Filter Frequency",
+		"Mic/Line Switch",
+		"Sampling Threshold",
+		"Multisample Switch",
+		"Omni Mode Flag",
+		"Midi Channel",
+		"Thru Mode Switch",
+		"Mod Enable Switch",
+		"Source Start MSB",
+		"Source Start LSB",
+		"Source End MSB",
+		"Source End LSB",
+		"Destination MSB",
+		"Destination LSB",
+		"Destination Bank",
+		"Scale Start Factor",
+		"Scale End Factor",
+		"External Computer Switch",
+		"Baud Rate Switch",
+		"Cartridge Filter Freq",
+		"Software Version",
+		"Spare" };
+
+	unsigned char ulprogsam = 0;
+	unsigned char paramnumber = 0;
+	unsigned char paramvalue = 0;
 	unsigned char * ptr;
 	DWORD BytesRecorded;
 	BOOL io_dir;
@@ -131,13 +166,23 @@ void CMidiMonThread::OnParseSysex(WPARAM wParam, LPARAM lParam)
 	unsigned char MessageID;
 	int i;
 
+	// Simple check for complete valid sysex
+	if ( *(ptr) != 0xF0 && *(ptr+(BytesRecorded-1)) != 0xF7 )
+	{
+		LocalFree(pcds);
+		return;
+	}
+
+	ulprogsam = *(ptr+4);
+
 	switch (*(ptr+3))
 	{
 		case CONFIG_PARM_REQ:
-			LogMessage += "Config Parameters Request";
+			LogMessage += "Config Parameters Request"; // Only 5 bytes, simple request
 			break;
+
 		case COMMAND_CODE:
-			LogMessage += "Command Code";
+			LogMessage += "Command Code"; // Needs expanding 
 			switch ( *(ptr+4))
 			{
 				case SELECT_LOWER:
@@ -148,72 +193,157 @@ void CMidiMonThread::OnParseSysex(WPARAM wParam, LPARAM lParam)
 					break;
 			}
 			break;
+
 		case CONFIG_PARM_DUMP:
-			LogMessage += "Config Parameters Dump Data";
+			LogMessage += "Config Parameters Dump Data"; // Needs more info
+			m_pMidiDoc->PutData(LogMessage, io_dir);
+			LogMessage.clear();
+			for ( int i=0 ; i < 28 ; i++ )
+			{
+				paramvalue = de_nybblify( *(ptr+4+(2*i)),*(ptr+5+(2*i)) );
+				sprintf(SEMessage,"Param # %d, %s %d", i+20, ConfigParams[i], paramvalue);
+				m_pMidiDoc->PutData(SEMessage, io_dir);
+			}
+			LogMessage += "End Config Parameters Dump Data";
 			break;
+
 		case LOWER_PRG_DUMP_REQ:
-			LogMessage += "Lower Program Dump Request";
+			LogMessage += "Lower Program Dump Request"; // Only 5 bytes, simple request
 			break;
+
 		case UPPER_PRG_DUMP_REQ:
-			LogMessage += "Upper Program Dump Request";
+			LogMessage += "Upper Program Dump Request"; // Only 5 bytes, simple request
 			break;
+
 		case WAVE_DUMP_REQ:
-			LogMessage += "Wave Dump Request";
+			LogMessage += "Wave Dump Request"; // Only 5 bytes, simple request. The sample is selected via wavesample select
 			break;
+
 		case PRG_DUMP_LOWER:
-			LogMessage += "Lower Program Dump Data";
+			LogMessage += "Lower Program Dump Data"; // Needs more info
 			break;
+
 		case PRG_DUMP_UPPER:
-			LogMessage += "Upper Program Dump Data";
+			LogMessage += "Upper Program Dump Data"; // Needs more info
 			break;
+
 		case WAVE_DUMP_DATA:
-			LogMessage += "Wave Dump Data";
+			LogMessage += "Wave Dump Data"; // No extra info needed.
 			break;
+
 		case PRG_STATUS_MSG:
-			LogMessage += "Program Status Message";
+			LogMessage += "Program Status Message"; // Send when a program number is changed from the front panel
+			m_pMidiDoc->PutData(LogMessage, io_dir);
+			LogMessage.clear();
+			if ( (ulprogsam&0xF0) == 0 )
+			{
+				sprintf(SEMessage, "Selected lower program %d", (ulprogsam&0xF)+1);
+			} else {
+				sprintf(SEMessage, "Selected upper program %d", (ulprogsam&0xF)+1);
+			}
+			LogMessage += SEMessage;
 			break;
+
 		case WAVE_STATUS_MSG:
-			LogMessage += "Wavesample Status Message";
+			LogMessage += "Wavesample Status Message"; // Send when a new wavesample is selected from the front panel
+			m_pMidiDoc->PutData(LogMessage, io_dir);
+			LogMessage.clear();
+			if ( (ulprogsam&0xF0) == 0 )
+			{
+				sprintf(SEMessage, "Selected lower sample %d", (ulprogsam&0xF)+1);
+			} else {
+				sprintf(SEMessage, "Selected upper sample %d", (ulprogsam&0xF)+1);
+			}
+			LogMessage += SEMessage;
 			break;
+
 		case WAVE_ACK:
-			LogMessage += "Wavesample acknowledge";
+			LogMessage += "Wavesample acknowledge"; // Sent by mirage when the checksum of a received sample is good. 
+																							// Also sent when a wavesample function is completed
 			break;
+
 		case WAVE_NACK:
 			LogMessage += "Wavesample NOT acknowleged";
 			break;
+
 		case WAVEDUMPABSREQ:
 			LogMessage += "Wavesample Dump Absolute Request";
 			break;
+
 		case WAVEDUMPABSDATA:
 			LogMessage += "Wavesample Dump Absolute Data";
 			break;
+
 		case PRG_PARM_MSG:
-			LogMessage += "Program Parameter Message";
+			LogMessage += "Program Parameter Message"; // Sent whenever a program parameter is changed from the front panel.
+			/* +4 = 000N00PP  where N=0 Lower, N=1 Upper, PP = Program Number
+				 +5 = parameter #
+				 +6 = LS Nybble
+				 +7 = MS Nybble
+			*/
+			paramnumber = *(ptr+5);
+			paramvalue = (*(ptr+6) + (*(ptr+7)<<4));
+			
+			if ( (ulprogsam&0xF0) == 0 )
+			{
+				sprintf(SEMessage, " for lower program %d ", (ulprogsam&0xF)+1);
+			} else {
+				sprintf(SEMessage, " for upper program %d ", (ulprogsam&0xF)+1);
+			}
+			LogMessage += SEMessage;
+			m_pMidiDoc->PutData(LogMessage, io_dir);
+			LogMessage.clear();
+			sprintf(SEMessage,"Parameter %02d : %02d", paramnumber, paramvalue);
+			LogMessage += SEMessage;
 			break;
+
 		case SMP_PARM_MSG:
-			LogMessage += "Wavesample Parameter Message";
+			LogMessage += "Wavesample Parameter Message"; // Sent whenever a sample parameter is changed from the front panel. 
+			/* +4 = 000N0SSS  where N=0 Lower, N=1 Upper, SSS = wavesample #
+				 +5 = parameter #
+				 +6 = LS Nybble
+				 +7 = MS Nybble
+			*/
+			paramnumber = *(ptr+5);
+			paramvalue = (*(ptr+6) + (*(ptr+7)<<4));
+			
+			if ( (ulprogsam&0xF0) == 0 )
+			{
+				sprintf(SEMessage, " for lower sample #%d ", (ulprogsam&0xF)+1);
+			} else {
+				sprintf(SEMessage, " for upper sample #%d ", (ulprogsam&0xF)+1);
+			}
+			LogMessage += SEMessage;
+			m_pMidiDoc->PutData(LogMessage, io_dir);
+			LogMessage.clear();
+			if ( paramnumber < 69 )
+			{
+				sprintf(SEMessage,"Parameter %02d : %02X", paramnumber, paramvalue);
+			} else {
+					sprintf(SEMessage,"Parameter %02d : %02d", paramnumber, paramvalue);
+			}
+			LogMessage += SEMessage;
 			break;
+
 		case WAVEMANIPCMD:
 			LogMessage += "Wavesample Manipulation Function Command";
 			break;
+
 		default:
 			LogMessage += "Unknown Mirage Sysex";
 			sprintf(SEMessage," Message ID: %02X ",*(ptr+3));
 			LogMessage += SEMessage;
 	}
-
-	if ( *(ptr) != 0xF0 && *(ptr+(BytesRecorded-1)) != 0xF7 )
-	{
-		LocalFree(pcds);
-		return;
-	}
 	
 	m_pMidiDoc->PutData(LogMessage, io_dir);
 	LogMessage.clear();
 
-	sprintf(SEMessage, "System Exclusive Size: %d", BytesRecorded);
-	m_pMidiDoc->PutData(LogMessage, io_dir);
-	LogMessage.clear();
+	if ( BytesRecorded >= 64 )
+	{
+		sprintf(SEMessage, "System Exclusive Size: %d", BytesRecorded);
+		m_pMidiDoc->PutData(SEMessage, io_dir);
+		LogMessage.clear();
+	}
 
 	if ( BytesRecorded > 47 ) BytesRecorded = 47;
 
